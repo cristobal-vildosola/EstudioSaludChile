97c5d381a35d9b831e5fe1d370ddf5fccefbf4c4
Cristobal Muñoz <cristobal.vildosola@gmail.com>
2019-08-20 12:24:02 -0400

    delete hello world

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Start from '@/components/Start'
 
 Vue.use(Router)
@@ -8,11 +7,6 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/',
       name: 'Start',
