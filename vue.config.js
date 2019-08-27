caf13c0ba1557fbb411dcea7cd51057fb7ba79ae
Cristobal Muñoz <cristobal.vildosola@gmail.com>
2019-08-27 12:07:47 -0400

    add travis integration for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '~crmunoz/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'EstudioSaludChile/' : '/',
   runtimeCompiler: true,
 }
